3d5446a74cd663ad36fc2c980dc14cfb5a8bdf56
Zach Gaskin <zgaskin@ufclab.com>
2019-11-24 12:11:52 -0500

    build function start

---
+++ functions/stpconnect/stpconnect.js
@@ -0,0 +1,14 @@
+exports.handler = async (event, context) => {
+  try {
+    const subject = event.queryStringParameters.name || "SWorld"
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}
